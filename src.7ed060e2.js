4dff390534e16d0f96cec4be2308c6d19dc13e29
Vitaly Reutsky <vreutsky77@mail.ru>
2022-05-16 08:30:46 +0300

    Updates

--- src.7ed060e2.js
+++ src.7ed060e2.js
@@ -5588,7 +5588,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55641" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52184" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
